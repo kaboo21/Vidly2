cca312407632a7dabf9a3b548bb786b5c7f24780
pavelkovasyl <pavelkovasyl@gmail.com>
2018-04-10 21:57:44 +0300

    Chapter2

--- Vidly2/App_Start/RouteConfig.cs
+++ Vidly2/App_Start/RouteConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +13,9 @@ namespace Vidly2
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapMvcAttributeRoutes();
+           
 
             routes.MapRoute(
                 name: "Default",
--- Vidly2/Controllers/MoviesController.cs
+++ Vidly2/Controllers/MoviesController.cs
@@ -4,16 +4,39 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly2.Models;
+using Vidly2.ViewModels;
 
 namespace Vidly2.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        // GET: Movies/Randome
         public ActionResult Random ()
         {
             var movie = new Movie() {Name = "Shrek!"};
-            return View(movie);
+            var customers =new List<Customer>
+            {
+                new Customer {Name="Customer 1"},
+                new Customer {Name="Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id" + id);
+        }
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year,int month)
+        {
+            return Content(year+"/"+month);
         }
     }
 }
