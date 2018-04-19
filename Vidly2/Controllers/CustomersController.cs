1074b9143d38a52786587c3d30ef96516b9a90fb
pavelkovasyl <pavelkovasyl@gmail.com>
2018-04-19 23:24:46 +0300

    Chapter 3 ex1

--- Vidly2/Controllers/CustomersController.cs
+++ Vidly2/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,29 +10,29 @@ namespace Vidly2.Controllers
 {
     public class CustomersController : Controller
     {
+        //call DB of the application
+        private ApplicationDbContext _context;
+
+        //assigning value to the _context in constructor
+        public CustomersController()
+        {
+            _context=new ApplicationDbContext();
+        }
         // GET: Customer
         public ActionResult Index()
         {
-            var customers=GetCustomers();
+            var customers=_context.Customers.Include(c=>c.MembershipType).ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             return View(customer);
 
         }
 
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer {Id = 1,Name = "Rob"},
-                new Customer {Id = 2,Name = "Jerry"}
-
-            };
-        }
+        
     }
 }
